eb62671d6f7ca19763be801c2e619ccf88fc7dfe
soohyeon_choi <soohyeon_choi@tmax.co.kr>
2025-03-24 16:15:18 +0900

    기능정의

---
+++ src/model/charge.js
@@ -0,0 +1,8 @@
+export class Charge {
+  constructor(coin500, coin100, coin50, coin10) {
+    this.coin500 = coin500;
+    this.coin100 = coin100;
+    this.coin50 = coin50;
+    this.coin10 = coin10;
+  }
+}
